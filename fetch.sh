e4c485c6617801917d9177b7ffb383849cfbe1f8
Merramore <Merramore@users.noreply.github.com>
2024-08-04 16:56:08 -0500

    common anti-randomization

--- fetch.sh
+++ fetch.sh
@@ -18,19 +18,40 @@ _with() { "${@}"; }
 
 
 
+common_randoms=(
+    -e 's/<meta name="sentry-trace"[^>]*>/<meta name="sentry-trace" \/>/g'
+    -e 's/<meta name="baggage"[^>]*>/<meta name="baggage" \/>/g'
+    -e 's/<input name="__RequestVerificationToken" type="hidden"[^>]*>/<input name="__RequestVerificationToken" type="hidden" \/>/g'
+    -e 's/email-protection#[^"]*/email-protection#/g'
+    -e 's/window.__CF$cv$params={[^\}]*}/window.__CF$cv$params={}/g'
+)
+strip_common_randoms() {
+    sed \
+      "${common_randoms[@]}" \
+      -i "${1}"
+}
+
+index_randoms=(
+    -e 's/star-63759-overall-[a-zA-Z0-9_-]*/star-63759-overall-/g'
+)
+strip_index_randoms() {
+    sed \
+      "${common_randoms[@]}" \
+      "${index_randoms[@]}" \
+      -i "${1}"
+}
+
+chapter_randoms=(
+    -e 's/<p class="c[^"]*"/<p class="c"/g'
+)
 strip_chapter_randoms() {
     local antitheft="$(grep -Poe '(?<=\.)c[a-zA-Z0-9_-]*(?=\{)' "${1}" || echo _cAntitheft)"
     # sed equivalent: -e 's/\.c[a-zA-Z0-9_-]\+{/\._cAntiTheft{b/g'
     sed \
       -e "s/${antitheft}/_cAntiTheft/g" \
       -e 's/<p class="_cAntiTheft".*\?<\/p[^>]*>//g' \
-      -e 's/<p class="c[^"]*"/<p class="c"/g' \
-      -e 's/<meta name="sentry-trace"[^>]*>/<meta name="sentry-trace" \/>/g' \
-      -e 's/<meta name="baggage"[^>]*>/<meta name="baggage" \/>/g' \
-      -e 's/<input name="__RequestVerificationToken" type="hidden"[^>]*>/<input name="__RequestVerificationToken" type="hidden" \/>/g' \
-      -e 's/email-protection#[^"]*/email-protection#/g' \
-      -e 's/window.__CF$cv$params={[^\}]*}/window.__CF$cv$params={}/g' \
-      -e 's/star-63759-overall-[a-zA-Z0-9_-]*/star-63759-overall-/g' \
+      "${common_randoms[@]}" \
+      "${chapter_randoms[@]}" \
       -i "${1}"
 }
 
@@ -143,6 +164,8 @@ case "${1}" in
     run_and_log) run_and_log;;
     run_and_tee_log) run_and_tee_log;;
     do_fetch) do_fetch;;
+    strip_common_randoms) strip_common_randoms "${2}";;
+    strip_index_randoms) strip_index_randoms "${2}";;
     strip_chapter_randoms) strip_chapter_randoms "${2}";;
     *) usage;;
 esac
